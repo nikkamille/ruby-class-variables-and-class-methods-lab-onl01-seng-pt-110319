c6d4f862d86fb55dd2f960fa804fd064ab188dae
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-27 01:08:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,10 @@ class Song
   def self.genre_count
     @@genre_count = {}
     @@genres each do |genre|
-      if genre_count[genre] += 1 
+      if genre_count[genre]
+        genre_count[genre] += 1 
     else
+      genre_count[genre] = 1 
   end
   
 end
