f822fd129ee16bc017303d09b88131753a46473b
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-27 00:57:22 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists 
+    @@artists.uniq 
+  end
+  
+  def self.genres 
+    @@genres = []
+  end
+  
+  def self.artists
+    @@artists = []
+  end
+  
+  def self.genre_count
+    @@genre_count = {}
+      
+  end
+  
+end
